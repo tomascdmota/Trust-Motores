a87c86b1b9e1892f08b12c4d0ef6a0382bd71239
tomascdmota <tomasmota@outlook.com>
2023-10-07 11:40:58 +0100

    Changed cors policies

--- server/server.js
+++ server/server.js
@@ -18,7 +18,16 @@ const port = process.env.PORT || 3001;
 // Middleware
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
+app.use(function (req, res, next) {
+    // Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+    );
+    next();
+  });
 
 
 // Your other routes and configurations here
